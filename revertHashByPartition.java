171ef652b3b8ad9ac5445f775b25bffbafcb6ddb
Richeve Bebedor <richeve.bebedor@gmail.com>
2014-08-08 15:43:59 +0800

    Emergency push.

---
+++ revertHashByPartition.java
@@ -0,0 +1,32 @@
+package revertHashByPartition;
+
+public class revertHashByPartition{
+
+    public static void main( String... parameterList ){
+        if( parameterList.length != 3 ){
+            Exception exception = new Exception( "invalid parameter list" );
+            System.err.print( exception.getMessage( ) );
+
+            return;
+        }
+
+        String hash = parameterList[ 0 ];
+        String dictionary = parameterList[ 1 ];
+
+        int length = 1;
+        try{
+            length = parameterList[ 2 ];
+        }catch( Exception exception ){
+            System.err.print( exception.getMessage( ) );
+
+            return;
+        }
+
+
+
+    }
+
+    public static final String revertHashByPartition(  ){
+
+    }
+}
